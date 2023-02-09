0767bc19f3d63201c94553e9ee271c56bea946cc
realtradam <github@tradam.dev>
2023-02-09 14:36:09 -0500

    added arguments and return example

---
+++ mrb_gems/arguments_and_return_example/mrbgem.rake
@@ -0,0 +1,4 @@
+MRuby::Gem::Specification.new('arguments_and_return_example') do |spec|
+  spec.license = 'MIT'
+  spec.author  = 'Tradam'
+end
--- test_suite.rb
+++ test_suite.rb
@@ -1,4 +1,14 @@
+puts "Beginning Test Suite..."
+puts
+
+puts " - Basic Example - "
 BasicExample.say_hello
 BasicExample.say_goodbye
+puts
+
+puts " - Multiply Numbers Return Example - "
+print "3 * 10 = "
+puts ArgumentsAndReturnExample.multiply_numbers(3, 10)
+puts
 
 puts "Reached end of test suite."
